6d5bd59c2920e27560f9b265220914b63929398e
MateuszDyla <mdyla6@gmail.com>
2024-06-16 21:59:10 +0200

    qr scanner implementation

--- frontend/src/pages/ScannerPage.jsx
+++ frontend/src/pages/ScannerPage.jsx
@@ -7,23 +7,8 @@ import "../styles/ScannerPageStyle.css";
 function ScannerPage() {
   const devices = useDevices();
   const [deviceIndex, setDeviceIndex] = useState(0); // Initialize deviceIndex to 0
-  const [qrData, setQrData] = useState(null);
   const navigate = useNavigate();
   const scannerRef = useRef(null); // Ref to Scanner component
-
-  useEffect(() => {
-    console.log("Devices:", devices);
-    console.log("Current Device Index:", deviceIndex);
-    console.log("Current Device ID:", devices[deviceIndex]?.deviceId);
-  }, [devices, deviceIndex]);
-
-  useEffect(() => {
-    // Reset scanner when deviceIndex changes
-    if (scannerRef.current) {
-      scannerRef.current.stop(); // Stop current scanner
-      scannerRef.current.start(); // Start with new deviceId
-    }
-  }, [deviceIndex]);
 
   const changeDevice = () => {
     if (devices.length <= 1) return;
@@ -32,8 +17,7 @@ function ScannerPage() {
 
   const onScan = (result) => {
     if (result) {
-      setQrData(result);
-      console.log("Scanned Result:", result);
+      console.log("Scanned Result:", result[0].rawValue);
     }
   };
 
@@ -45,15 +29,13 @@ function ScannerPage() {
             aspectRatio: { ideal: 1 },
             facingMode: { ideal: "environment" },
             focusMode: { ideal: "continuous" },
+            deviceId: devices[deviceIndex]?.deviceId,
           }}
           onScan={onScan}
-          deviceId={devices[deviceIndex]?.deviceId || null}
         />
       </div>
       <div id="scanner-utilities">
-        <div className="scanner-output">
-          {qrData && <p className="scanning-status">Scanned: {qrData}</p>}
-        </div>
+        <div className="scanner-output">sadasdasd</div>
         <div className="scanner-buttons">
           <div className="scan-button-left">
             <DarkButton onClick={() => navigate(-1)}>Powrót</DarkButton>
