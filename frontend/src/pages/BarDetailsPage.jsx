ad6253df35283ebc7acc392450d812c7b9d350ed
StaraMyrta <105817809+StaraMyrta@users.noreply.github.com>
2024-06-18 23:41:11 +0200

    textbox-komentarze

--- frontend/src/pages/BarDetailsPage.jsx
+++ frontend/src/pages/BarDetailsPage.jsx
@@ -7,96 +7,95 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { DarkButton } from "../Components/DarkButton";
 
 function BarDetailsPage() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const queryParameters = new URLSearchParams(location.search);
-  const id = queryParameters.get("id");
-  const userId = localStorage.getItem("userId");
-  const token = localStorage.getItem("token");
+    const navigate = useNavigate();
+    const location = useLocation();
+    const queryParameters = new URLSearchParams(location.search);
+    const id = queryParameters.get("id");
+    const userId = localStorage.getItem("userId");
+    const token = localStorage.getItem("token");
 
-  const [barInfo, setBarInfo] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const [barInfo, setBarInfo] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    navigate("/");
-  };
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/");
+    };
 
-  useEffect(() => {
-    setLoading(true);
-    fetch(`http://localhost:8080/bars/${id}/user/${userId}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`error: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setBarInfo(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("error :", error);
-        setError(error.message);
-        setLoading(false);
-      });
-  }, [id, userId, token]);
+    useEffect(() => {
+        setLoading(true);
+        fetch(`http://localhost:8080/bars/${id}/user/${userId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`error: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setBarInfo(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error("error :", error);
+                setError(error.message);
+                setLoading(false);
+            });
+    }, [id, userId, token]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
 
-  if (!barInfo) {
-    return <div>No bar information to get</div>;
-  }
+    if (!barInfo) {
+        return <div>No bar information to get</div>;
+    }
 
-  return (
-    <>
-      <Header onLogout={handleLogout} />
-      <div className="tiny-logo-container">
-        <Logo />
-      </div>
-      <div id="bar-details">
-        <div id="bar-details-image">
-          <img src={barInfo.bar.imageUrl} />
-        </div>
-        <div id="bar-details-text">
-          <p className="bar-name-profile">{barInfo.bar.name}</p>
-          <p className="bar-address-profile">{barInfo.bar.address}</p>
-          <span>Łącznie odwiedzony {barInfo.allVisits} razy</span>
-          <br />
-          <span>Odwiedzony przez Ciebie {barInfo.visitsByUser} razy</span>
-          <br />
-        </div>
-      </div>
-      <div id="comment-wrapper">
-        <div id="textarea-comment">
-          <textarea></textarea>
-        </div>
-        <div id="button-comment">
-          <DarkButton onClick={() => navigate("/odwiedzone-bary")}>
-            <i class="fa-regular fa-comment"></i>
-          </DarkButton>
-        </div>
-      </div>
-
-      <div className="center" width="80%">
-        <CommentCanva id="5" />
-        <DarkButton onClick={() => navigate("/odwiedzone-bary")}>
-          Powrót
-        </DarkButton>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Header onLogout={handleLogout} />
+            <div className="tiny-logo-container">
+                <Logo />
+            </div>
+            <div id="bar-details">
+                <div id="bar-details-image">
+                    <img src={barInfo.bar.imageUrl} alt="Bar" />
+                </div>
+                <div id="bar-details-text">
+                    <p className="bar-name-profile">{barInfo.bar.name}</p>
+                    <p className="bar-address-profile">{barInfo.bar.address}</p>
+                    <span>Łącznie odwiedzony {barInfo.allVisits} razy</span>
+                    <br />
+                    <span>Odwiedzony przez Ciebie {barInfo.visitsByUser} razy</span>
+                    <br />
+                </div>
+            </div>
+            <div className="center">
+                <CommentCanva id="5" />
+                <div id="comment-wrapper">
+                    <div id="textarea-comment">
+                        <textarea></textarea>
+                    </div>
+                    <div id="button-comment">
+                        <DarkButton onClick={() => navigate("/odwiedzone-bary")}>
+                            <i className="fa-regular fa-comment"></i>
+                        </DarkButton>
+                    </div>
+                </div>
+                <DarkButton onClick={() => navigate("/odwiedzone-bary")}>
+                    Powrót
+                </DarkButton>
+            </div>
+        </>
+    );
 }
 
 export default BarDetailsPage;
