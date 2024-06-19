ec76d4d713491802ce20a63a4dc431dfa795383a
MateuszDyla <mdyla6@gmail.com>
2024-06-19 12:46:42 +0200

    change to cors

--- backend/src/main/java/com/beereal/beerealbackend/config/WebConfig.java
+++ backend/src/main/java/com/beereal/beerealbackend/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5173", "psim-backend.azurewebsites.net"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
